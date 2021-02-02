daa32f348003900b1571b40e83844d825f1f4c33
zhangchuangqi <1075688050@qq.com>
2021-02-02 10:23:36 +0800

    first commit

---
+++ src/components/layout/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <el-container>
+  <el-header class="header">
+    <div class="header-main">
+      <img src="../../assets/images/icon.jpg" alt="">
+      <ul>
+        <li
+          v-for="item in navMenu"
+          :key="item.name"
+          :class="navMenuIndex === item.index ? 'active' : ''"
+          @click="navClick(item)">
+            {{ item.title }}
+          </li>
+      </ul>
+      <ul>
+        <li>个人中心</li>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+    </div>
+  </el-header>
+  <el-container>
+    <el-aside class="aside">
+      <el-menu>
+      </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+import menu from '../../utils/menu.json'
+export default {
+  data () {
+    return {
+      menu: [
+        {
+          title: '选项卡一',
+          id: 0,
+          items: [
+            {
+              title: '栏目一',
+              id: 1
+            },
+            {
+              title: '栏目二',
+              id: 2
+            }
+          ]
+        },
+        {
+          title: '选项卡二',
+          id: 3,
+          items: [
+            {
+              title: '栏目一',
+              id: 4
+            }
+          ]
+        }
+      ],
+      navMenu: [],
+      items: [],
+      navMenuIndex: 0
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      // console.log(menu)
+      // this.menu = menu[0]
+      // console.log(this.menu)
+      // this.items = this.menu.map(v => {
+      //   return v[0].items
+      // })
+      this.navMenu = menu.map(v => {
+        return {
+          index: v.index,
+          name: v.name,
+          path: v.path,
+          title: v.title
+        }
+      })
+    },
+    navClick (item) {
+      this.navMenuIndex = item.index
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import './index.scss'
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
 
+// 导入样式表
+import './assets/css/reset.scss'
+import './assets/css/base.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
